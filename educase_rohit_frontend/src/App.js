77d6d59907a44683abe30e87ce05d26180ece334
Rohit-Palavanchu <rohitp.vja@gmail.com>
2025-06-05 21:06:48 +0530

    implemented the profile view and login page

--- educase_rohit_frontend/src/App.js
+++ educase_rohit_frontend/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import CreateAccount from "./components/CreateAccount";
+import LoginPage from "./components/LoginPage";
+import ProfileView from "./components/ProfileView";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfileView />} />
       </Routes>
     </Router>
   );
